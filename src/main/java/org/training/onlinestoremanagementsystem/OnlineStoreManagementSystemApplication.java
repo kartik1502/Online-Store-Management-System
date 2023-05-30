8926bb954b11529c6a01ed9f99aa128ba2131bc9
kartik1502 <kartikkulkarni1411@gmail.com>
2023-05-30 12:59:09 +0530

    Initial commit

---
+++ src/main/java/org/training/onlinestoremanagementsystem/OnlineStoreManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.training.onlinestoremanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/training/onlinestoremanagementsystem/OnlineStoreManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training.onlinestoremanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
